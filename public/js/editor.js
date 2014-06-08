546ba99c84870c4acbcfb757f273f9078ec4c51d
dcfvg <qwsx@dcfvg.com>
2014-06-08 12:12:14 +0200

    add editor feedback

--- public/js/editor.js
+++ public/js/editor.js
@@ -6,14 +6,43 @@ function init() {
 
   $('#sendNote').on('click', sendNewNote);
 
+  socket.on('newImage', onNewImage);
+
+
+  function onNewImage(data) {
+    console.log('new image :: data', data);
+    var $newimage = $("<div>")
+      .addClass("col-xs-2")
+      .append($('<img>')
+        .addClass('thumb')
+        .attr('alt', data.note.text)
+        .attr('src', data.src)
+      );
+
+    console.log($newimage);
+    console.log("article#"+data.note.time);
+    $newimage.appendTo("article#t"+data.note.time);
+  };
+
   function sendNewNote(e){
-    socket.emit("newNote",
-    {
+    var data =  {
       session : app.session,
       text : $("#note").val(),
       column : $("#column").val(),
-      time : Math.round((new Date()).getTime() / 1000)
-    });
-  }
+      time : Math.round((new Date()).getTime())
+    }
+    socket.emit("newNote",data);
+    addNewNote(data);
+  };
+  function addNewNote(data){
+
+    var $newline = $('<article>')
+        .addClass('record row lead')
+        .attr('id', 't'+data.time)
+        .append($('<p>')
+          .addClass('legend col-xs-3')
+          .html(data.text));
+    $newline.appendTo('#content');
+  };
 };
 $(document).on('ready', init);
