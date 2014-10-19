c96de4a78a179a27ca568e9ff82d4eb869a26b08
dcfvg <qwsx@dcfvg.com>
2014-10-19 12:29:59 +0200

    screenshots + dir create

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var glob = require("glob");
 var path = require("path");
 var gm = require('gm');
 var markdown = require( "markdown" ).markdown;
+var exec = require('child_process').exec;
 
 module.exports = function(app, io){
   console.log("main module initialized");
@@ -14,23 +15,25 @@ module.exports = function(app, io){
   var images = [];
   var checkImagesInterval;
 
+  var spawn = require('child_process').spawn;
+
   io.on("connection", function(socket){
 
     socket.on("newLine", onNewLine);
     socket.on("newUser", onNewUser);
-
+    socket.on("newRecipe", onNewRecipe)
 
   });
 
   function init(){
-
+    onNewRecipe();
   };
 
   // events
 
   function onNewUser(req){
     console.log(req);
-  }
+  };
   function onNewLine(req){
     console.log(req);
 
@@ -42,10 +45,15 @@ module.exports = function(app, io){
     recordSessionLines(req.session, lines);
     io.sockets.emit("incomingLine", req);
   };
+  function onNewRecipe(req){
+    var session  ="A"
+    var recipeId = glob(sessions_p+'/'+session+'/*.png', {nocase: true, sync: true}).length;
 
-  // reload from files
-
-
+    exec('screencapture -x '+sessions_p+'/'+session+'/'+recipeId+'.png', function(error, stdout, stderr){ 
+      console.log("capture");
+      io.sockets.emit("newRecipeId", {recipeId: recipeId});
+    });
+  }
 
   // helpers
   function pad(num, size) {
@@ -53,6 +61,7 @@ module.exports = function(app, io){
     while (s.length < size) s = "0" + s;
     return s;
   }
+  function execute(command, callback){ exec(command, function(error, stdout, stderr){ callback(stdout); });};
   function timestampToTimer(time){
 
       var d = new Date(time);
