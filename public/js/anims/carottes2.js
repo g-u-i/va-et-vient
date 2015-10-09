f1655dcaa3e4e1badcfbb8423cb81ba37880d276
sarah garcin <sarah@g-u-i.me>
2015-10-09 15:05:16 +0200

    finish animation accompagnements

--- public/js/anims/carottes2.js
+++ public/js/anims/carottes2.js
@@ -2,30 +2,21 @@ var center = view.center;
 var width = view.size.width;
 var height = view.size.height;
 
-function Champignon(_radius, _destination, _position){
+function Carotte(_radius, _destination, _position){
   this.radius = _radius;
   this.destination = _destination;
   this.position = _position;
 
   // CREATE THE SHAPE
-  this.path = new Path({
-    fillColor: '#3e3217',
-  });
-
-  var point1 = new Segment(new Point(50, 40), new Point(-5, -10), new Point(0, 0));
-  var point2 = new Segment(new Point(70, 30), new Point(0, 0), new Point(10, 18));
-  var point3 = new Segment(new Point(50, 90), new Point(25, -9), new Point(-20, 9));
-  var point4 = new Segment(new Point(-5, 70), new Point(10, 20), new Point(-10, -20));
-  var point5 = new Segment(new Point(15, 22), new Point(-15, 5), new Point(15, -5));
-
-  this.path.add(point1, point2, point3, point4, point5);
-  this.path.closed = true;
+  //var rectangle = new Rectangle(new Point(50, 50), new Point(150, 100));
+  this.path = new Path.Rectangle(new Point(50, 50), new Point(250, 100));
+  this.path.fillColor = '#e94e1b';
   this.path.position = this.position;
-  this.path.scale(this.radius);
+  this.path.scale(0.8);
   this.path.opacity = 0.8;
 }
 
-Champignon.prototype = {
+Carotte.prototype = {
   move: function(event, i){
     var vector = this.destination - this.position;
     this.position += vector /100;
@@ -43,18 +34,18 @@ Champignon.prototype = {
 
 //--------------------- main ---------------------
 
-var champignons = [];
-var number = 3;
+var carottes = [];
+var number = 5;
 
 for (var i = 0; i < number; i++) {
   var destination = Point.random() * view.size;
   var position = Point.random() * view.size;
   var radius = Math.random() * 3 + 1;
-  champignons.push(new Champignon(radius, destination, position));
+  carottes.push(new Carotte(radius, destination, position));
 }
 
 function onFrame(event) {
-  for (var i = 0, l = champignons.length; i < l; i++) {
-    champignons[i].move(event, i);
+  for (var i = 0, l = carottes.length; i < l; i++) {
+    carottes[i].move(event, i);
   }
 }
--- public/js/anims/fromage.js
+++ public/js/anims/fromage.js
@@ -2,30 +2,23 @@ var center = view.center;
 var width = view.size.width;
 var height = view.size.height;
 
-function Champignon(_radius, _destination, _position){
+function Fromage(_radius, _destination, _position){
   this.radius = _radius;
   this.destination = _destination;
   this.position = _position;
 
   // CREATE THE SHAPE
   this.path = new Path({
-    fillColor: '#3e3217',
+    fillColor: '#f9d638',
   });
-
-  var point1 = new Segment(new Point(50, 40), new Point(-5, -10), new Point(0, 0));
-  var point2 = new Segment(new Point(70, 30), new Point(0, 0), new Point(10, 18));
-  var point3 = new Segment(new Point(50, 90), new Point(25, -9), new Point(-20, 9));
-  var point4 = new Segment(new Point(-5, 70), new Point(10, 20), new Point(-10, -20));
-  var point5 = new Segment(new Point(15, 22), new Point(-15, 5), new Point(15, -5));
-
-  this.path.add(point1, point2, point3, point4, point5);
+  this.path.add([30, 30], [92, 28], [110,65],[20, 68]);
   this.path.closed = true;
   this.path.position = this.position;
   this.path.scale(this.radius);
   this.path.opacity = 0.8;
 }
 
-Champignon.prototype = {
+Fromage.prototype = {
   move: function(event, i){
     var vector = this.destination - this.position;
     this.position += vector /100;
@@ -43,18 +36,18 @@ Champignon.prototype = {
 
 //--------------------- main ---------------------
 
-var champignons = [];
+var fromages = [];
 var number = 3;
 
 for (var i = 0; i < number; i++) {
   var destination = Point.random() * view.size;
   var position = Point.random() * view.size;
-  var radius = Math.random() * 3 + 1;
-  champignons.push(new Champignon(radius, destination, position));
+  var radius = Math.random() * 2 + 1;
+  fromages.push(new Fromage(radius, destination, position));
 }
 
 function onFrame(event) {
-  for (var i = 0, l = champignons.length; i < l; i++) {
-    champignons[i].move(event, i);
+  for (var i = 0, l = fromages.length; i < l; i++) {
+    fromages[i].move(event, i);
   }
 }
