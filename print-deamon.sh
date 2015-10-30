95a6af45028a4487251d375c1578deb62552bb8e
dcfvg <qwsx@dcfvg.com>
2015-10-30 18:45:47 +0100

    new deamon using watch

--- print-deamon.sh
+++ print-deamon.sh
@@ -2,28 +2,17 @@
 # set -x
 
 # settings
+# lanch with $ watch -n 10 print-deamon.sh 
 
-printer=Canon_LBP7100C_7110C                    # labos
+printer=Canon_LBP7100C_7110C
 archivebox="archivebox/"
 printinbox="printbox/"
-interval=3
 
 # main loop
 mkdir $archivebox $printinbox
 
-while true; do
-
-  for step in `find $printinbox -iname "*.pdf" -type f`
-  do 
-    lpr -P $printer -o media=A4 -o fit-to-page $step
-    mv -v $step $archivebox # copy in outbox (archives)
-  done
-
-  # wait
-  for (( i=$interval; i>0; i--)); do
-    sleep 1 &
-    printf "next try in $i s \r"
-    wait
-    printf "                   \r"
-  done
+for step in `find $printinbox -iname "*.pdf" -type f`
+do 
+  lpr -P $printer -o media=A4 -o fit-to-page $step
+  mv -v $step $archivebox # copy in outbox (archives)
 done
